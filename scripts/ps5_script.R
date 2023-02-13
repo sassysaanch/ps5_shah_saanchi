95fb31e24af72b08fa2993c2d29358c995f4f925
SAANCHI SHAH <sshah15@g.ucla.edu>
2023-02-12 19:24:35 -0800

    add u of alabama plot on dev

--- scripts/ps5_script.R
+++ scripts/ps5_script.R
@@ -16,14 +16,29 @@ library(tidyverse)
 ## ---------------------------
 ## directory paths
 ## ---------------------------
-
+plots_dir <- file.path("./plots")
 
 
 ## -----------------------------------------------------------------------------
-## Part X - Label each question using comments
+## Part 2 - Label each question using comments
 ## -----------------------------------------------------------------------------
+load(url("https://github.com/anyone-can-cook/rclass2/raw/main/data/recruiting/recruit_school_somevars.RData"))
 
+# + `visits_by_100751` = University of Alabama
+#+ `visits_by_126614` = University of Colorado Boulder
+# + `visits_by_110635` = UC Berkeley
 
+df_univ <- df_school %>% 
+  mutate(visited = if_else(visits_by_100751 >= 1, 1, 0)) %>% 
+  filter(state_code == inst_100751) %>% 
+  select(ncessch, total_students, avgmedian_inc_2564, visited)
+
+png(file.path(plots_dir, 'scatterplot_alabama.png'))
+ggplot(data = df_univ, aes(x = total_students, y = avgmedian_inc_2564, color = as.factor(visited))) +
+  geom_point() +
+  xlab('Total enrollment') + ylab('Average median income') +
+  scale_color_discrete(name = 'Recruitment Visits', labels = c('No visits', 'Visits'))
+dev.off()
 
 ## -----------------------------------------------------------------------------
 ## END SCRIPT
