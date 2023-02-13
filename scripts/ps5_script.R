bc783e6367cdd4c4def08e4e458012260d603523
SAANCHI SHAH <sshah15@g.ucla.edu>
2023-02-12 19:29:33 -0800

    add cu boulder plot on dev

--- scripts/ps5_script.R
+++ scripts/ps5_script.R
@@ -29,11 +29,11 @@ load(url("https://github.com/anyone-can-cook/rclass2/raw/main/data/recruiting/re
 # + `visits_by_110635` = UC Berkeley
 
 df_univ <- df_school %>% 
-  mutate(visited = if_else(visits_by_100751 >= 1, 1, 0)) %>% 
-  filter(state_code == inst_100751) %>% 
+  mutate(visited = if_else(visits_by_126614 >= 1, 1, 0)) %>% 
+  filter(state_code == inst_126614) %>% 
   select(ncessch, total_students, avgmedian_inc_2564, visited)
 
-png(file.path(plots_dir, 'scatterplot_alabama.png'))
+png(file.path(plots_dir, 'scatterplot_cuboulder.png'))
 ggplot(data = df_univ, aes(x = total_students, y = avgmedian_inc_2564, color = as.factor(visited))) +
   geom_point() +
   xlab('Total enrollment') + ylab('Average median income') +
